ecaf9556151b00481627c19c4f9affea2a78f89d
kubi2021 <92210118+kubi2021@users.noreply.github.com>
2024-11-05 20:33:13 +0100

    skip undesired genres

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,6 @@ from unittest.mock import MagicMock
 
 # Mock xbmc and other related modules
 sys.modules['xbmc'] = MagicMock()
+sys.modules['xbmcaddon'] = MagicMock()
 sys.modules['xbmcgui'] = MagicMock()
+sys.modules['xbmcplugin'] = MagicMock()
