8eec2fd161fe57a4803cb60d0dd6f643ab1030cf
kubi2021 <92210118+kubi2021@users.noreply.github.com>
2025-07-21 21:33:45 +0200

    fixed tests

--- tests/plugin_video_mubi/test_library.py
+++ tests/plugin_video_mubi/test_library.py
@@ -385,7 +385,7 @@ def test_sync_locally(mock_remove_obsolete, mock_prepare_files, mock_dialog_prog
         library.sync_locally(base_url, plugin_userdata_path, omdb_api_key)
 
         # Assertions for progress dialog
-        mock_dialog.create.assert_called_once_with("Syncing with MUBI", "Starting the sync...")
+        mock_dialog.create.assert_called_once_with("Syncing with MUBI 2/2", "Starting the sync...")
         assert mock_dialog.update.call_count == 2, "Progress dialog should have been updated twice"
         mock_dialog.close.assert_called_once()
 
@@ -559,7 +559,7 @@ def test_sync_locally_empty_library(mock_remove_obsolete, mock_dialog_progress, 
         library.sync_locally(base_url, plugin_userdata_path, omdb_api_key)
 
         # Assertions for progress dialog
-        mock_dialog.create.assert_called_once_with("Syncing with MUBI", "Starting the sync...")
+        mock_dialog.create.assert_called_once_with("Syncing with MUBI 2/2", "Starting the sync...")
         mock_dialog.update.assert_not_called()
         mock_dialog.close.assert_called_once()
 
